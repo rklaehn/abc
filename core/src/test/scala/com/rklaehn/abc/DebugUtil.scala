3e5bbd9819477daffd92065d87c513ccd0cdf3a0
Rüdiger Klaehn <rklaehn@gmail.com>
2015-12-12 19:18:55 +0100

    Removed Thyme from DebugUtils
    It is now in a separate project

--- core/src/test/scala/com/rklaehn/abc/DebugUtil.scala
+++ core/src/test/scala/com/rklaehn/abc/DebugUtil.scala
@@ -1,11 +1,6 @@
 package com.rklaehn.abc
 
-import ichi.bench.Thyme
-import ichi.bench.Thyme.HowWarm
-
 object DebugUtil {
-
-  lazy val th = Thyme.warmed(verbose = println, warmth = HowWarm.BenchOff)
 
   implicit class IsSpecializedExtension(private val ref: AnyRef) extends AnyVal {
 
