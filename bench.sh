a0507d5bd5fbb6f04a68e6ac3b160aebf4194789
Rüdiger Klaehn <rklaehn@gmail.com>
2015-12-22 08:53:27 +0100

    Added more benchmarks to default runner script

--- bench.sh
+++ bench.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sbt "jmhBenchmarks/jmh:run -i 5 -wi 5 -f1 -t1 -rf csv -rff sonicreducer.csv SonicReducerBench"
+sbt "jmhBenchmarks/jmh:run -i 5 -wi 5 -f1 -t1 -rf csv -rff setcreate.csv SetCreateBench"
 sbt "jmhBenchmarks/jmh:run -i 5 -wi 5 -f1 -t1 -rf csv -rff setelement.csv SetElementBench" 
 sbt "jmhBenchmarks/jmh:run -i 5 -wi 5 -f1 -t1 -rf csv -rff setset.csv SetSetBench" 
 gnuplot plot.gnu
